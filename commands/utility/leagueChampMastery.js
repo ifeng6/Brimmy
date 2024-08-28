ddb5b8c381a43ef3c7ab1252e8ad006f24d6ef30
ifeng6 <ivan.hu25@gmail.com>
2024-08-28 03:14:55 -0700

    add option to get top X masteries, up to 25

--- commands/utility/leagueChampMastery.js
+++ commands/utility/leagueChampMastery.js
@@ -12,6 +12,7 @@ const getMasteries = async (interaction) => {
     const platform = interaction.options.getString('platform')
     const gameName = interaction.options.getString('gamename')
     const tagLine = interaction.options.getString('tagline')
+    const count = interaction.options.getInteger('count') || 3 // By default, return top 3 masteries
 
     // Fetch PUUID 
     try {
@@ -23,7 +24,7 @@ const getMasteries = async (interaction) => {
             throw new Error('Player not found!')
         }
         // Get Champion Mastery information
-        const masteryResponse = await fetch(`https://${platform}/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/top?api_key=${riotAPIKey}`)
+        const masteryResponse = await fetch(`https://${platform}/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/top?count=${count}&api_key=${riotAPIKey}`)
         const responseJSON = await masteryResponse.json()
         if (!responseJSON) {
             throw new Error('Error loading masteries.')
@@ -32,8 +33,8 @@ const getMasteries = async (interaction) => {
         // Return list of top champion masteries (API already returns in descending order)
         const res = []
         let i = 1
-        responseJSON.forEach(element => {
-            const { championId, championLevel, championPoints } = element
+        responseJSON.forEach(championData => {
+            const { championId, championLevel, championPoints } = championData
             const champName = champNameFromID(championId.toString())
             res.push(`${i}. Champion: ${champName} | Mastery Level: ${championLevel} | Mastery Points: ${championPoints}`)
             i++
@@ -48,6 +49,7 @@ export const leagueChampMastery = {
     data: new SlashCommandBuilder()
         .setName('league-masteries')
         .setDescription('See your top champion masteries')
+        // Option for region (used with ign and tagline to find puuid)
         .addStringOption(option =>
             option
                 .setName('region')
@@ -58,6 +60,7 @@ export const leagueChampMastery = {
                     { name: "Asia", value: riotRegionalRouting.ASIA },
                     { name: "Europe", value: riotRegionalRouting.EUROPE }
                 ))
+        // Option for platform (used to find masteries based on puuid)
         .addStringOption(option =>
             option
                 .setName('platform')
@@ -81,15 +84,25 @@ export const leagueChampMastery = {
                     { name: "TW2", value: riotPlatformRouting.TW2 },
                     { name: "VN2", value: riotPlatformRouting.VN2 },
                 ))
+        // Option for IGN
         .addStringOption(option =>
             option
                 .setName('gamename')
                 .setDescription('In-game name')
                 .setRequired(true))
+        // Option for riot tagline
         .addStringOption(option =>
             option
                 .setName('tagline')
                 .setDescription('Riot tagline ex. NA1')
-                .setRequired(true)),
+                .setRequired(true))
+        // Option for number of top masteries to display
+        .addIntegerOption(option => 
+            option
+                .setName('count')
+                .setDescription('Count of top x masteries to display')
+                .setMinValue(1)
+                .setMaxValue(25)
+        ),
     execute: getMasteries,
 }
